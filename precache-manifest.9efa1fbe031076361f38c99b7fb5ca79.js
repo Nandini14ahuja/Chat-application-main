e3d1a2ec123def7978e144fbda673334aaef0348
nandini14ahuja <nandini14ahuja@gmail.com>
2021-07-11 17:19:42 +0530

    Updates

---
+++ precache-manifest.9efa1fbe031076361f38c99b7fb5ca79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fffa459f0c8f1b235a01",
+    "url": "/Chat-application-main/static/css/main.7283278a.chunk.css"
+  },
+  {
+    "revision": "fffa459f0c8f1b235a01",
+    "url": "/Chat-application-main/static/js/main.e27ab476.chunk.js"
+  },
+  {
+    "revision": "b15b2b30f5bbbac7aa5e",
+    "url": "/Chat-application-main/static/js/runtime~main.31d78172.js"
+  },
+  {
+    "revision": "0a545d22d4c8d41b8d75",
+    "url": "/Chat-application-main/static/css/2.6cc95706.chunk.css"
+  },
+  {
+    "revision": "0a545d22d4c8d41b8d75",
+    "url": "/Chat-application-main/static/js/2.c95413d8.chunk.js"
+  },
+  {
+    "revision": "c91e5be43123656fd67a171a253424e2",
+    "url": "/Chat-application-main/index.html"
+  }
+];
